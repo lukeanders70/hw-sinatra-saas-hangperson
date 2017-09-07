fdd4d746e20a0ce663b5cc7c84932b8463fdbf02
Luke Larson <luke.larson@berkeley.edu>
2017-09-07 19:43:28 +0000

    hangman tests passed

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -10,8 +10,62 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
+  
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
 
+  def guess(letter)
+    raise ArgumentError.new("must be a letter") unless letter =~ /^[abcdefghijklmnopqrstuvwxyz]/i
+    letter = letter.downcase
+    ret_val = true
+    
+    if (@guesses =~ /#{letter}/) or (@wrong_guesses =~ /#{letter}/) # if we've already seen letter
+      ret_val = false
+      
+    elsif !(@word =~ /#{letter}/) #if word does not contain letter
+      @wrong_guesses = @wrong_guesses + letter
+      
+    else
+      @guesses = @guesses + letter
+    end
+    
+    return ret_val
+  end
+    
+  def word_with_guesses()
+    dis = ''
+    @word.each_char do |l|
+      if (@guesses =~ /#{l}/)
+        dis += l
+      else
+        dis += '-'
+      end
+    end
+    return dis
+  end
+  
+  def check_win_or_lose()
+    won = true
+    @word.each_char do |l|
+      if !(@guesses =~ /#{l}/)
+        won = false
+      end
+    end
+    
+    if (won)
+      return :win
+    elsif (@wrong_guesses.length >= 7)
+      return :lose
+    else
+      return :play
+    end
+  end
+    
+  
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
